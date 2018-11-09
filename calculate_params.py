0fc272ed0fb90f3c09aa67ed4afc853369823fd3
Manuel Castro <manuel.ant.castro@gmail.com>
2018-11-09 17:12:09 +0100

    Computing the second block

--- calculate_params.py
+++ calculate_params.py
@@ -3,34 +3,16 @@ from utils import DataSheet
 from utils import SearchInColumns
 from oauth2client.service_account import ServiceAccountCredentials
 
-
-
 ATP_COL = 19
 H_COL = 26
 MRNA_ROW_END = 285
 
 
-def compute_tscr_elo_term_TU0_xxxx(sheet):
-    datos_sh = sheet.data_sheet
-    nucleotid_seq_sh = sheet.parent.worksheet('Secuencia nucleotidica')
-    search = SearchInColumns(nucleotid_seq_sh, 'A3:A282')
-    search.look_for(datos_sh, 'AO4:AO8')
-    results = search.get_values('B')
-    atp = sum((seq.count('A') for seq in results))
-    print(atp)
-
-
 def calculate_tscr_elo_term_TUB0_xxxx(sheet):
-    datos_sh = sheet.data_sheet
-    tu_rows = []
-    for cell in datos_sh.range('A4:A282'):
-        if 'TU_' in cell.value.upper():
-            tu_rows.append(cell.row)
-    end_ranges = [row - 1 for row in tu_rows]
-    end_ranges = end_ranges[1:]
-    end_ranges.append(MRNA_ROW_END)
+    tu_rows, end_ranges = get_tu_limits(sheet)
     nucleotid_seq_sh = sheet.parent.worksheet('Secuencia nucleotidica')
     searcher = SearchInColumns(nucleotid_seq_sh, 'A3:B282', 1)
+    datos_sh = sheet.data_sheet
     searcher.preload_cells(datos_sh, 'AM4:AM285')
     for init, end in zip(tu_rows, end_ranges):
         results = searcher.search(init, end)
@@ -52,24 +34,46 @@ def calculate_tscr_elo_term_TUB0_xxxx(sheet):
         print('From row {} to row {}'.format(init, end))
 
 
+def calculate_tl_elo_xxxx_1_rib1(sheet):
+    tu_rows, end_ranges = get_tu_limits(sheet)
+    amino_seq_sh = sheet.parent.worksheet('Secuencia aminoacidica')
+    searcher = SearchInColumns(amino_seq_sh, 'A3:B282', 1)
+    datos_sh = sheet.data_sheet
+    searcher.preload_cells(datos_sh, 'AM4:AM285')
+    computation_cell_range = datos_sh.range('AW4:BR282')
+    import ipdb; ipdb.set_trace()
+    for init, end in zip(tu_rows, end_ranges):
+        index = 0
+        results = searcher.search(init, end)
+        for letter in 'ARNDEGHILKMFPSTYVCWQA':
+           pep = sum((seq[1].count(letter) for seq in results))
+           
+        
+
+def get_tu_limits(sheet):
+    datos_sh = sheet.data_sheet
+    tu_rows = []
+    for cell in datos_sh.range('A4:A282'):
+        if 'TU_' in cell.value.upper():
+            tu_rows.append(cell.row)
+    end_ranges = [row - 1 for row in tu_rows]
+    end_ranges = end_ranges[1:]
+    end_ranges.append(MRNA_ROW_END)
+
+    return tu_rows, end_ranges
+
+
 def main():
     scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('transcriptional-model-key.json', scope)
-
+             'https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.\
+        from_json_keyfile_name('transcriptional-model-key.json', scope)
     gc = gspread.authorize(credentials)
-    # Open a worksheet from spreadsheet with one shot
     spreadsheet = gc.open("ematix_05_10")
     wk = spreadsheet.worksheet('Datos')
     sheet = DataSheet(spreadsheet, wk)
-    # compute_tscr_elo_term_TU0_xxxx(sheet)
-    calculate_tscr_elo_term_TUB0_xxxx(sheet)
-    # wks.update_acell('B2', "it's down there somewhere, let me take another look.")
-    # Fetch a cell range
-    # cell_list = wks.range('A1:B7')
-    # print(cell_list)
-
+    # calculate_tscr_elo_term_TUB0_xxxx(sheet)
+    calculate_tl_elo_xxxx_1_rib1(sheet)
 
 if __name__ == '__main__':
     main()
